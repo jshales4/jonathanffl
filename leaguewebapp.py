4f69bcba6cb4d9e951fb09ca715725e03dcbc780
Jonathan Shales <jshales46@gmail.com>
2017-09-16 23:06:06 -0700

    initial commit

---
+++ leaguewebapp.py
@@ -0,0 +1,16 @@
+##leaguewebapp.py
+from flask import Flask, render_template
+from espnff import League
+
+app = Flask(__name__)
+
+@app.route("/")
+def ranks():
+    return render_template('index.html')
+
+@app.route("/ranks")
+def rank():
+    return render_template('ranks.html',league=League(1140354, 2017).power_rankings(week=2))
+  
+if __name__ == "__main__":
+  app.run()
